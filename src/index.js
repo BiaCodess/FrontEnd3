514d621fdf0865fb690f12aff827b0318479c22d
BiaCodess <pereiracoelhobeatriz@gmail.com>
2024-11-27 18:18:54 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import CrudUsuarios from './components/CrudUsuarios';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <CrudUsuarios />
+  </React.StrictMode>
+);
+
+
